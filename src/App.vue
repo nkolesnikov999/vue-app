45292610f1b0860a55bb658b00052fbc0b2835e7
nkolesnikov999 <nkolesnikov999@gmail.com>
2025-06-09 10:37:27 +0300

    Init config

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+const color = 'red';
+const id = "blue";
+</script>
+
+<template>
+  <header :class="`color-${id}`">
+    <h1>My App</h1>
+  </header>
+  <main :class="color">
+    <h2>Welcome to My App</h2>
+  </main>
+  <footer>
+    <p>Copyright 2025</p>
+  </footer>
+</template>
+
+<style scoped>
+</style>
